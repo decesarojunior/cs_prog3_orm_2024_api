ceb0d4c877a46c06cb41414920dd3d7774480386
decesarojunior <decesarojunior@yahoo.com.br>
2024-12-03 12:20:31 -0300

    implementacoes em 03/12/2024

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express'; //importacao do roteador
 
 import JogadorController from './app/controllers/JogadorController';
+import PatenteController from './app/controllers/PatenteController';
 
 const router = Router(); // inicialização
 
@@ -10,6 +11,12 @@ router.post('/loginjogador', JogadorController.login);
 router.post('/insertjogador', JogadorController.store);
 router.get('/deletejogador/:nickname', JogadorController.delete);
 router.post('/updatejogador/', JogadorController.update);
+
+router.get('/listpatente', PatenteController.list);
+router.get('/patente/:id', PatenteController.find);
+router.get('/deletepatente/:id', PatenteController.delete);
+router.post('/updatepatente/', PatenteController.update);
+router.post('/insertpatente/', PatenteController.store);
 
 
 export default router; // exportacao da variável router (assim ele fica disponível para ser importada)
